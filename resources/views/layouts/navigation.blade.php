95043a393c624cdabf9f1ddd8db3517b31af0ee6
EwoiGift <2105065@students.kcau.ac.ke>
2025-08-04 15:12:15 +0300

    Update with logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,10 +5,16 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
+    <a href="{{ route('dashboard') }}">
+        <span class="text-2xl font-extrabold font-oswald logo-no-space">
+            <span class="text-gray-900">D</span>
+            <span class="text-red-700">R</span>
+            <span class="text-green-700">P</span>
+            <span class="text-gray-900">N</span>
+            <span class="text-red-700">K</span>
+        </span>
+    </a>
+</div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
